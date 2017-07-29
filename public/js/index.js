ca7b8058bfe330873ea84f19eb109d90a4c28d8c
AKALIA Slimane <slimaneakalia@gmailcom>
2017-07-29 13:25:15 +0000

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -12,6 +12,7 @@ function renderTemplate(templateId, obj)
 	var template = jQuery('#'+templateId).html();
 	var html = Mustache.render(template, obj);
 	jQuery('#messages').append(html);
+	scrollToBottom();
 }
 
 socket.on('newMessage', function(message){
@@ -62,3 +63,17 @@ sendLocation.on('click', function(){
 		alert('Unable to fetch geolocation');
 	});
 });
+
+function scrollToBottom(){
+	var messages = jQuery('#messages');
+	var newMessage = messages.children('li:last-child');
+
+	var clientHeight = messages.prop('clientHeight');
+	var scrollTop = messages.prop('scrollTop');
+	var scrollHeight = messages.prop('scrollHeight');
+	var newMessageHeight = newMessage.innerHeight();
+	var lastMessageHeight = newMessage.prev().innerHeight();
+
+	if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight)
+		messages.scrollTop(scrollHeight);
+}
